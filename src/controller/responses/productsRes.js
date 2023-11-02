9b620df9d612e6ca35fbab25c44aae7362128f75
fabianromeroclaros <fabian.romero.9385580@usalesiana.edu.bo>
2023-11-02 18:15:11 -0400

    feat: get products by categories and subcategories

--- src/controller/responses/productsRes.js
+++ src/controller/responses/productsRes.js
@@ -12,8 +12,131 @@ export const getProductById = async (request, response) => {
 };
 
 /**
- * Gets a list of incidents as a JSON response using pagination by .
+ * Gets a list of products as a JSON response using pagination.
 
- * @param {*} request - The request object.
- * @param {*} response - The response object.
- */
+ * @param {*} req - The request object.
+ * @param {*} res - The response object.
+*/
+export const getAllProducts = async (req, res) => {
+    const { page = 1, limit = 6 } = req.query;
+    try {
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+  
+      const products = await Product.find().skip(startIndex).limit(limit);
+      const totalProductsCount = await Product.countDocuments();
+  
+      const pagination = {
+        total: totalProductsCount,
+        limit: limit,
+        page: page
+      };
+  
+      if (endIndex < totalProductsCount) {
+        pagination.next = {
+          page: page + 1
+        };
+      }
+  
+      if (startIndex > 0) {
+        pagination.prev = {
+          page: page - 1
+        };
+      }
+  
+      res.status(200).json({
+        products,
+        pagination
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+}
+
+/**
+ * Gets a list of products as a JSON response using pagination by category.
+
+ * @param {*} req - The request object.
+ * @param {*} res - The response object.
+*/
+export const getAllProductsByCategory = async (req, res) => {
+    const { page = 1, limit = 6 } = req.query;
+    const { categoryId } = req.params;
+  
+    try {
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+  
+      const products = await Product.find({ category: categoryId }).skip(startIndex).limit(limit);
+      const totalProductsCount = await Product.countDocuments({ category: categoryId });
+  
+      const pagination = {
+        total: totalProductsCount,
+        limit: limit,
+        page: page
+      };
+  
+      if (endIndex < totalProductsCount) {
+        pagination.next = {
+          page: page + 1
+        };
+      }
+  
+      if (startIndex > 0) {
+        pagination.prev = {
+          page: page - 1
+        };
+      }
+  
+      res.status(200).json({
+        products,
+        pagination
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+}
+
+/**
+ * Gets a list of products as a JSON response using pagination by category and subcategory.
+
+ * @param {*} req - The request object.
+ * @param {*} res - The response object.
+*/
+export const getAllProductsByCategoryAndSubcategory = async (req, res) => {
+    const { page = 1, limit = 6 } = req.query;
+    const { categoryId, subcategoryId } = req.params;
+  
+    try {
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+  
+      const products = await Product.find({ category: categoryId, subcategory: subcategoryId }).skip(startIndex).limit(limit);
+      const totalProductsCount = await Product.countDocuments({ category: categoryId, subcategory: subcategoryId });
+  
+      const pagination = {
+        total: totalProductsCount,
+        limit: limit,
+        page: page
+      };
+  
+      if (endIndex < totalProductsCount) {
+        pagination.next = {
+          page: page + 1
+        };
+      }
+  
+      if (startIndex > 0) {
+        pagination.prev = {
+          page: page - 1
+        };
+      }
+  
+      res.status(200).json({
+        products,
+        pagination
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+};
