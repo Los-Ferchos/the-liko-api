17d9b46c07c50b50868466c43efb644ebeef6d80
santiCaballero <santiago.caballero.6760385@usalesiana.edu.bo>
2023-11-08 11:05:10 -0400

    Review, And total Review sorting, Docs implemented

--- src/controller/methods/sort.js
+++ src/controller/methods/sort.js
@@ -1,5 +1,15 @@
+
+/**
+ * 
+ * @param {number} sortNumber Sort number 
+ * 1, -1 .- For Name Sorting
+ * 2, -2 .- For Price Sorting
+ * 3, -3 .- For Rating Sorting
+ * 4, -4 .- For Total Reviews Sorting
+ * @returns the key value for the sort way
+ */
 export const getSortTypeField = (sortNumber) => {
-    let sortField = 'asdaf';
+    let sortField = 'name';
 
     switch (sortNumber) {
         case '1':
@@ -13,6 +23,18 @@ export const getSortTypeField = (sortNumber) => {
             break;
         case '-2':
             sortField = 'price';
+            break;
+        case '3':
+            sortField = 'rating';
+            break;
+        case '-3':
+            sortField = 'rating';
+            break;
+        case '4':
+            sortField = 'totalReviews';
+            break;
+        case '-4':
+            sortField = 'totalReviews';
             break;
         default:
             break;
