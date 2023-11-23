75ab4803357451497be1cc61795f2cf8aa544710
Fernando Mauricio Mamani Navarro <fernando.mamani.13389201@usalesiana.edu.bo>
2023-11-22 23:09:40 -0400

    fix: fixing bug 20

--- src/routes/checkout.routes.js
+++ src/routes/checkout.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getCodeCheckout, processPayment } from '../controller/requests/checkoutReq.js';
+import { getCodeCheckout, processPayment, validateUserInformation} from '../controller/requests/checkoutReq.js';
 
 const checkoutRouter = Router();
 
 checkoutRouter.post('/codeCheckout', getCodeCheckout );
 checkoutRouter.post('/confirmCheckout', processPayment );
+checkoutRouter.post('/validateUserInformation', validateUserInformation );
 
 
 export default checkoutRouter;
