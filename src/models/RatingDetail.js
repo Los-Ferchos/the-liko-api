00042449f2cc9c0ee1d7fe1564c80fe9867c0c88
Fernando Mauricio Mamani Navarro <fernando.mamani.13389201@usalesiana.edu.bo>
2023-11-28 11:37:34 -0400

    doc: documentation

--- src/models/RatingDetail.js
+++ src/models/RatingDetail.js
@@ -1,5 +1,19 @@
 import { Schema, model } from 'mongoose';
 
+/**
+ * RatingEschema represents the schema for rating details of a product.
+ *
+ * @typedef {Object} RatingEschema
+ * @property {string} productId - The ID of the product.
+ * @property {number} rating_1 - The count of rating 1 for the product.
+ * @property {number} rating_2 - The count of rating 2 for the product.
+ * @property {number} rating_3 - The count of rating 3 for the product.
+ * @property {number} rating_4 - The count of rating 4 for the product.
+ * @property {number} rating_5 - The count of rating 5 for the product.
+ * @property {number} totalRating - The total count of ratings for the product.
+ * @property {Date} createdAt - The timestamp when the rating details were created.
+ * @property {Date} updatedAt - The timestamp when the rating details were last updated.
+ */
 const RatingEschema = new Schema({
     productId: { 
       type: String, 
